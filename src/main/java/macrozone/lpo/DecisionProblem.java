af6091d1a7659690e98302b0d8cb9b3460d39175
Constantin Lazari <cola@Lenovo.(none)>
2013-09-12 20:30:59 +0200

    First Interface for the application.

---
+++ src/main/java/macrozone/lpo/DecisionProblem.java
@@ -0,0 +1,21 @@
+/*
+ * File: 		DecisionProblem.java
+ * Date: 		Sep 12, 2013
+ *
+ * Copyright 2013 Datura Informatik + Organisation AG. All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, this software
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.
+ */
+package macrozone.lpo;
+
+/**
+ * Description of a decision problem
+ */
+public interface DecisionProblem {
+
+	String getTitle();
+	
+	void setTitle(final String title);
+}
